2c8e87ded98ed1647c8ab01b0a7e443c95de8c38
SayyehBan <sdvp1991david@gmail.com>
2024-08-03 18:34:27 +0330

    پویا کردن دکمه

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { CURRENTLINE, CYAN, ORANGE, PURPLE, RED } from "../../helpers/colors";
 const Contact = () => {
+  const [hoverEye, setHoverEye] = useState(false);
+  const [hoverTrash, setHoverTrash] = useState(false);
   return (
     <div className="col-md-6">
       <div style={{ backgroundColor: CURRENTLINE }} className="card my-2">
@@ -34,14 +37,36 @@ const Contact = () => {
               </ul>
             </div>
             <div className="col-md-1 col-sm-1 d-flex flex-column align-items-center">
-              <button className="btn my-1" style={{ backgroundColor: ORANGE }}>
-                <i className="fa fa-eye" />
-              </button>{" "}
+              <button
+                className="btn my-1"
+                style={{ backgroundColor: ORANGE }}
+                onMouseEnter={() => setHoverEye(true)}
+                onMouseLeave={() => setHoverEye(false)}
+              >
+                <i
+                  className={`${
+                    hoverEye === false
+                      ? "fa-solid fa-arrows-to-eye"
+                      : "fa-solid fa-eye"
+                  }`}
+                />
+              </button>
               <button className="btn my-1" style={{ backgroundColor: CYAN }}>
                 <i className="fa fa-pen" />
               </button>
-              <button className="btn my-1" style={{ backgroundColor: RED }}>
-                <i className="fa fa-trash" />
+              <button
+                className="btn my-1"
+                style={{ backgroundColor: RED }}
+                onMouseEnter={() => setHoverTrash(true)}
+                onMouseLeave={() => setHoverTrash(false)}
+              >
+                <i
+                  className={`${
+                    hoverTrash === false
+                      ? "fa-solid fa-trash"
+                      : "fa-solid fa-trash-can-arrow-up"
+                  }`}
+                />
               </button>
             </div>
           </div>
