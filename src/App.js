5901086eb723da5d09cdc22696f09f3a87ac2406
SayyehBan <sdvp1991david@gmail.com>
2025-02-07 19:54:18 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h3>اپلیکشن مدیریت مخاطبین</h3>
+      <span className="fa fa-user"></span>
+      <span className="fas fa-address-book"></span>
+    </div>
+  );
+}
+
+export default App;
